0e8c22e0fa348277ed41843a7a2ab33e582dade6
LRB <>
2019-05-25 19:05:32 +1200

    New projects

---
+++ book_recommender/main.py
@@ -0,0 +1,53 @@
+# page: https://towardsdatascience.com/building-a-book-recommendation-system-using-keras-1fba34180699
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import seaborn as sns
+
+from keras.layers import Input, Embedding, Flatten, Dot, Dense
+from keras.models import Model
+
+dataset = pd.read_csv('data/ratings.csv')
+train, test = train_test_split(dataset, test_size=0.2, random_state=42)
+n_users = len(dataset.user_id.unique())
+n_books = len(dataset.book_id.unique())
+
+book_input = Input(shape=[1], name="Book-Input")
+book_embedding = Embedding(n_books+1, 5, name="Book-Embedding")(book_input)
+book_vec = Flatten(name="Flatten-Books")(book_embedding)
+
+user_input = Input(shape=[1], name="User-Input")
+user_embedding = Embedding(n_users+1, 5, name="User-Embedding")(user_input)
+user_vec = Flatten(name="Flatten-Users")(user_embedding)
+
+prod = Dot(name="Dot-Product", axes=1)([book_vec, user_vec])
+
+model = Model([user_input, book_input], prod)
+model.compile('adam', 'mean_squared_error')
+
+history = model.fit([train.user_id, train.book_id], train.rating, epochs=10, verbose=1)
+model.save('regression_model.h5')
+
+# Extract embeddings
+book_em = model.get_layer('Book-Embedding')
+book_em_weights = book_em.get_weights()[0]
+
+pca = PCA(n_components=2)
+pca_result = pca.fit_transform(book_em_weights)
+sns.scatterplot(x=pca_result[:,0], y=pca_result[:,1])
+
+tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+tnse_results = tsne.fit_transform(book_em_weights)
+sns.scatterplot(x=tnse_results[:,0], y=tnse_results[:,1])
+
+# Creating dataset for making recommendations for the first user
+book_data = np.array(list(set(dataset.book_id)))
+user = np.array([1 for i in range(len(book_data))])
+predictions = model.predict([user, book_data])
+predictions = np.array([a[0] for a in predictions])
+recommended_book_ids = (-predictions).argsort()[:5]
+print(recommended_book_ids)
+print(predictions[recommended_book_ids])
---
+++ extreme_rare_event_classification/main.py
@@ -0,0 +1,1 @@
+# page: https://towardsdatascience.com/lstm-autoencoder-for-extreme-rare-event-classification-in-keras-ce209a224cfb
